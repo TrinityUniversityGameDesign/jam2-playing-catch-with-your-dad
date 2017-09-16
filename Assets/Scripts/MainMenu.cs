eefc0009b06e5455dcec5f1a0b6fb28eacc32c7c
Lauren Truong <lauren@laurens-mbp.trinity.local>
2017-09-16 14:33:26 -0500

    MenuScene commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	public Canvas quitmenu;
+	public Button play;
+	public Button exit;
+
+	// Use this for initialization
+	void Start () {
+
+		quitmenu = quitmenu.GetComponent<Canvas> (); 
+		play = play.GetComponent<Button> ();
+		exit = exit.GetComponent<Button> ();
+		quitmenu.enabled = false;
+	}
+
+	public void ExitPress(){
+
+		quitmenu.enabled = true;
+		play.enabled = false;
+		exit.enabled = false;
+	}
+
+	public void StartLevel(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void ExitGame(){
+		Application.Quit (); 
+	}
+
+}
